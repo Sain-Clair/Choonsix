8abf65b5abcb5caa2af31ed1097d4926e14ff08b
ysh <yoonsh1028@naver.com>
2023-12-21 00:45:39 +0900

    첫 커밋

---
+++ src/views/hook-demo/use-fullscreen-loading.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { useFullscreenLoading } from "@/hooks/useFullscreenLoading"
+import { getSuccessApi, getErrorApi } from "@/api/hook-demo/use-fullscreen-loading"
+import { ElMessage } from "element-plus"
+
+const svg = `
+  <path class="path" d="
+    M 30 15
+    L 28 17
+    M 25.61 25.61
+    A 15 15, 0, 0, 1, 15 30
+    A 15 15, 0, 1, 1, 27.99 7.5
+    L 15 15
+  " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
+`
+
+const options = {
+  text: "即将发生错误...",
+  background: "#F56C6C20",
+  svg,
+  svgViewBox: "-10, -10, 50, 50"
+}
+
+const querySuccess = async () => {
+  // 注意：
+  // 1. getSuccessApi 是一个函数而非函数调用
+  // 2. 如需给 getSuccessApi 函数传递参数，请在后面的括号中进行（真正的 getSuccessApi 调用）
+  const res = await useFullscreenLoading(getSuccessApi)([2, 3, 3])
+  ElMessage.success(`${res.message}，传参为 ${res.data.list.toString()}`)
+}
+
+const queryError = async () => {
+  try {
+    await useFullscreenLoading(getErrorApi, options)()
+  } catch (err: any) {
+    ElMessage.error(err.message)
+  }
+}
+</script>
+
+<template>
+  <div class="app-container">
+    <h4>춘식이 로딩</h4>
+    <el-button type="primary" @click="querySuccess">춘삼</el-button>
+    <el-button type="danger" @click="queryError">사춘짜장</el-button>
+  </div>
+</template>
---
+++ src/views/hook-demo/use-watermark.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import { ref } from "vue"
+import { useWatermark } from "@/hooks/useWatermark"
+
+const localRef = ref<HTMLElement | null>(null)
+const { setWatermark, clearWatermark } = useWatermark(localRef)
+const { setWatermark: setGlobalWatermark, clearWatermark: clearGlobalWatermark } = useWatermark()
+</script>
+
+<template>
+  <div class="app-container">
+    <h4>귀여운 춘식이</h4>
+    <div ref="localRef" class="local" />
+    <el-button-group>
+      <el-button type="primary" @click="setWatermark('局部水印', { color: '#409eff' })"
+        >워터마크에 나타난 춘식이</el-button
+      >
+      <el-button type="warning" @click="setWatermark('没有防御功能的局部水印', { color: '#e6a23c', defense: false })">
+        워터마크 춘식이
+      </el-button>
+      <el-button type="danger" @click="clearWatermark">워터마크에 나타난 춘삼이</el-button>
+    </el-button-group>
+    <el-button-group>
+      <el-button type="primary" @click="setGlobalWatermark('全局水印', { color: '#409eff' })">낄낄</el-button>
+      <el-button
+        type="warning"
+        @click="setGlobalWatermark('没有防御功能的全局水印', { color: '#e6a23c', defense: false })"
+      >
+        다이어트춘
+      </el-button>
+      <el-button type="danger" @click="clearGlobalWatermark">다이어트 춘</el-button>
+    </el-button-group>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.local {
+  height: 30vh;
+  border: 2px dashed var(--el-color-primary);
+  margin-bottom: 20px;
+}
+
+.el-button-group {
+  margin-right: 12px;
+}
+</style>
